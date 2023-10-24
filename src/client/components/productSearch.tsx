1770bef90aed1195a37d3110f9f6a1fc3757fc1d
studiomfb <57592762+studioMFB@users.noreply.github.com>
2023-10-24 01:33:11 +0100

    app completed for initial test release.

--- src/client/components/productSearch.tsx
+++ src/client/components/productSearch.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
-import { useLazyQuery, useQuery } from '@apollo/client';
+import "./../styles/main.scss";
+import "./../styles/product.scss";
+import { useState } from 'react';
+import { useLazyQuery } from '@apollo/client';
 import { IProduct } from '../../server/schema/product/product.model';
 import { PRODUCTS_QUERY } from "../../server/schema/product/product.queries";
 import { ERROR_MSG_WRONG } from '../../constants/error';
@@ -9,7 +11,6 @@ function ProductSearch() {
     const [make, setMake] = useState('');
     const [model, setModel] = useState('');
     const [colour, setColour] = useState('');
-    const [price, setPrice] = useState('');
 
     const [getProducts, { loading, data, error }] = useLazyQuery(PRODUCTS_QUERY);
 
@@ -18,7 +19,6 @@ function ProductSearch() {
         if (make) filter.make = make;
         if (model) filter.model = model;
         if (colour) filter.colour = colour;
-        if (price) filter.price = Number(price);
 
         // Execute the query manually with the current filter
         getProducts({ variables: { filter } });
@@ -32,7 +32,6 @@ function ProductSearch() {
             <input type="text" placeholder="Make" value={make} onChange={(e) => setMake(e.target.value)} />
             <input type="text" placeholder="Model" value={model} onChange={(e) => setModel(e.target.value)} />
             <input type="text" placeholder="Colour" value={colour} onChange={(e) => setColour(e.target.value)} />
-            <input type="text" placeholder="Price" value={price} onChange={(e) => setPrice(e.target.value)} />
 
             <button onClick={search}>Search</button>
             {data && (
